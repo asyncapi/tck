a195044e2538cfbba183b0f75dce7b7d07aed5f7
Artem Kostiuk <postatum@gmail.com>
2020-06-29 18:51:26 +0300

    Fixes #18: Add more parsers to the reports (#19)
    * Copy and rework a little Go and Java runners
    
    * Fix few obvious Java runner issues
    
    * Remove unused Java imports
    
    * Make an initial Go runner rework
    
    * Fix and rename Go runner
    
    * Remove valkyr file

---
+++ runner/java/README.md
@@ -0,0 +1,37 @@
+## About
+
+Simple test of few AsyncAPI Java parsers. Tests simply try to parse a set of examples and report if parser returned an error.
+
+Running tests produces JSON reports.
+
+A fine collection of AsyncAPI files can be composed each containing one/few AsyncAPI features to test those in isolation.
+
+Uses [AsyncAPI/tck](https://github.com/asyncapi/tck/tests/asyncapi-2.0) as a source of AsyncAPI for tests.
+
+NOTE: If file name contains "invalid" parsing of it is expected to fail.
+
+## Install
+
+```sh
+$ git clone git@github.com:asyncapi/tck.git
+$ cd tck/runner/java
+$ ./gradlew build
+```
+
+## Run
+
+```sh
+$ ./gradlew run --args='--parser PARSER_NAME --outdir ./reports/json'
+```
+
+## Options
+
+Parser:
+```sh
+$ ./gradlew run --args='--parser amf'
+```
+
+Output JSON report directory (defaults to `./`):
+```sh
+$ ./gradlew run --args='--parser amf --outdir ../reports/json'
+```
---
+++ runner/java/src/main/java/org/asyncapi/runner/AsyncapiTckRunner.java
@@ -0,0 +1,103 @@
+package org.asyncapi.runner;
+
+import org.asyncapi.parsers.IParser;
+import org.asyncapi.parsers.AmfParser;
+import org.asyncapi.utils.Utils;
+
+import picocli.CommandLine;
+import picocli.CommandLine.ParameterException;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Stream;
+
+
+@Command(name = "asyncapi-tck-runner", mixinStandardHelpOptions = true, version = "1.0.0")
+public class AsyncapiTckRunner implements Runnable {
+  @Option(names = "--parser", description = "name of a parser to run")
+  String parserName;
+
+  @Option(names = "--outdir", description = "output JSON report directory")
+  String outdir = "./";
+
+  public IParser pickParser() {
+    IParser parser;
+    switch (parserName) {
+      case "amf":
+        parser = new AmfParser();
+        break;
+      default:
+        throw new ParameterException(
+          new CommandLine(this),
+          "Not supported parser: " + parserName);
+    }
+    return parser;
+  }
+
+
+  /**
+   * Parsers meta-data which helps generating pretty reports.
+   * Required fields are: name, language, url, version.
+   * Name and language are used in links creation.
+   */
+  public JSONObject pickParserMeta() {
+    JSONObject parserMeta = new JSONObject();
+    parserMeta.put("name", parserName);
+    parserMeta.put("language", "java");
+    switch (parserName) {
+      case "amf":
+        parserMeta.put("url", "https://github.com/aml-org/amf");
+        parserMeta.put("version", "4.1.2");
+        break;
+      default:
+        throw new ParameterException(
+          new CommandLine(this),
+          "Not supported parser: " + parserName);
+    }
+    return parserMeta;
+  }
+
+  public void run() {
+    IParser parser = this.pickParser();
+    JSONObject parserMeta = this.pickParserMeta();
+
+    // Get asyncapi/tck root path. When running via Makefile paths are relative to
+    // tck/runner/java
+    Path rootDirRel = Paths.get("../../");
+    String rootDir = rootDirRel.toAbsolutePath().toString();
+    String testsDir = Paths.get(rootDir, "tests", "asyncapi-2.0").toString();
+    Stream<String> fileList = Utils.listYamls(testsDir);
+
+    JSONObject report = new JSONObject();
+    report.put("parser", parserMeta);
+    JSONArray results = new JSONArray();
+
+    fileList.forEach(fpath -> {
+      Boolean success = true;
+      String error = "";
+      try {
+        parser.parse(fpath);
+      } catch (Exception e) {
+        success = false;
+        error = e.getMessage();
+      }
+      JSONObject result = new JSONObject();
+      result.put("file", fpath.replaceAll(rootDir, ""));
+      result.put("success", success);
+      result.put("error", error);
+      results.add(result);
+    });
+
+    report.put("results", results);
+    Utils.saveReport(report, outdir);
+  }
+
+  public static void main(String[] args) {
+    CommandLine.run(new AsyncapiTckRunner(), args);
+  }
+}
